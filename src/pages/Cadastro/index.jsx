80260a93a83f7768022c4eec89f0bb2a451d98a3
jacoryan-dev <jacoryan83@gmail.com>
2025-07-27 12:41:20 -0300

    feat(user): envia os dados do formulário para a API e redireciona para página de login

--- src/pages/Cadastro/index.jsx
+++ src/pages/Cadastro/index.jsx
@@ -1,57 +1,76 @@
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import { useRef } from "react";
+import api from "../../services/api";
 
 function Cadastro() {
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
   const navigate = useNavigate();
-
-  const handleSubmit = (event) => {
+  async function handleSubmit(event) {
     event.preventDefault();
-    // lógica de cadastro
-    // após o cadastro, redirecionar para a página de login
-    navigate("/login");
-  };
+
+    try {
+      // Enviar os dados do formulário para a API
+      await api.post("/cadastro", {
+        name: nameRef.current.value,
+        email: emailRef.current.value,
+        password: passwordRef.current.value,
+      });
+      alert("Cadastro realizado com sucesso!");
+      navigate("/login");
+    } catch (error) {
+      alert("Erro ao cadastrar:", error);
+    }
+  }
 
   return (
-    <div className="mx-auto mt-4 max-w-md bg-white border  border-gray-50 p-8 rounded-lg shadow-lg">
-      <h1 className="text-2xl text-center font-bold mb-6">Cadastro</h1>
-      <form onSubmit={handleSubmit} className="space-y-4">
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            id="name"
-            className="w-full px-3 py-2 border border-gray-300 rounded-md"
-          />
-        </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            className="w-full px-3 py-2 border border-gray-300 rounded-md"
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            className="w-full px-3 py-2 border border-gray-300 rounded-md"
-          />
-        </div>
-        <button
-          type="submit"
-          className="w-full bg-blue-600 text-white py-2 px-4 mt-4 rounded-md hover:bg-blue-500"
+    <div className="bg-blue-100 min-h-screen flex items-center justify-center">
+      <div className="mx-auto w-full mt-4 max-w-md bg-white border  border-gray-50 p-8 rounded-lg shadow-lg">
+        <h1 className="text-2xl text-center font-bold mb-6">Cadastro</h1>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div>
+            <label htmlFor="name">Nome:</label>
+            <input
+              type="text"
+              id="name"
+              ref={nameRef}
+              className="w-full px-3 py-2 border border-gray-300 rounded-md"
+            />
+          </div>
+          <div>
+            <label htmlFor="email">E-mail:</label>
+            <input
+              type="email"
+              id="email"
+              ref={emailRef}
+              className="w-full px-3 py-2 border border-gray-300 rounded-md"
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Senha:</label>
+            <input
+              type="password"
+              id="password"
+              ref={passwordRef}
+              className="w-full px-3 py-2 border border-gray-300 rounded-md"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-600 text-white py-2 px-4 mt-4 rounded-md hover:bg-blue-500"
+          >
+            Cadastrar
+          </button>
+        </form>
+        <Link
+          to="/login"
+          className="block text-center mt-4 text-sm text-blue-500 hover:underline"
         >
-          Cadastrar
-        </button>
-      </form>
-      <Link
-        to="/login"
-        className="block text-center mt-4 text-sm text-blue-500 hover:underline"
-      >
-        Já possui uma conta? Faça login
-      </Link>
+          Já possui uma conta? Faça login
+        </Link>
+      </div>
     </div>
   );
 }
